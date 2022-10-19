b360ca467e80d8a5b186f1456608512c3d26e400
Michaeel12 <114030156+Michaeel12@users.noreply.github.com>
2022-10-19 20:03:51 +0200

    Rekrutacja

--- tasker/src/json/Exporter.py
+++ tasker/src/json/Exporter.py
@@ -10,7 +10,7 @@ class Exporter:
 
     def save_tasks(self,tasks):
         self.tasks = tasks
-        with open('D:/PythonProjekty/Projects/taski.json','w') as file:
+        with open('taski.json','w') as file:
             json.dump(self.tasks, file)
         # TODO zapisz taski do pliku tutaj
 
